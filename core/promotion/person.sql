de8b16032cf417cf23b8742f8dd61d12861e74e3
Frank Lan <franklan118@gmail.com>
2015-09-29 21:17:08 +1000

    user account for Mushfique

--- core/promotion/person.sql
+++ core/promotion/person.sql
@@ -2,4 +2,5 @@ TRUNCATE TABLE `person`;
 INSERT INTO `person` (`id`, `firstName`, `lastName`, `email`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
 (10, 'System', 'User', NULL, 1, '2014-03-06 19:47:35', 10, '2015-08-22 00:14:32', 10),
 (24, 'Frank', 'Lan', NULL, 1, '2014-12-20 13:23:44', 20, '2015-08-22 00:14:37', 20),
+(25, 'Mushfique', '', NULL, 1, '2014-12-20 13:23:44', 20, '2015-08-22 00:14:37', 20),
 (2514, 'test', 'user', NULL, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24);
