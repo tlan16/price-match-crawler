68020fe80eda9d770f98cc24c98bb48a9795c414
murahman2008 <murahman2008@gmail.com>
2015-10-06 00:40:14 +1100

    adding a class to hold all the constants to identify a page controller

---
+++ core/main/utils/PageHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This class contains all the constants that can be used to identify each page/controller i.e. ProductListController
+ * 
+ * @author Mushfique
+ *
+ */
+class PageHandler
+{
+	const PRODUCT_LISTING_PAGE = 1;
+	
+	const PRODUCT_DETAILS_PAGE = 2;
+	
+	const PRODUCT_EDIT_PAGE = 3;
+	
+	const PRODUCT_DELETE_PAGE = 4;
+	
+	const MATERIAL_LISTING_PAGE = 5;
+}
