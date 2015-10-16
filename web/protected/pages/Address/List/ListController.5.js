32b8530183d92c0efc5ff02d77d7ed38bd4679e6
Frank <tlan16@sina.com>
2015-10-16 19:59:21 +1100

    clean up

--- web/protected/pages/Address/List/ListController.5.js
+++ web/protected/pages/Address/List/ListController.5.js
@@ -70,7 +70,6 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 	,_getResultRow: function(row, isTitle) {
 		var tmp = {};
 		tmp.me = this;
-		console.debug(row);
 		tmp.isTitle = (isTitle || false);
 		tmp.tag = (tmp.isTitle === true ? 'strong' : 'span');
 		tmp.row = new Element('span', {'class': 'row'})
