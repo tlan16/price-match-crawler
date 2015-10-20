c5765619a94ac708f56c1d1269a906899f2376a1
helin16 <helin16@192.168.200.202>
2015-10-20 18:16:08 +1100

    added productinfotype

--- core/promotion/materialinfotype.sql
+++ core/promotion/materialinfotype.sql
@@ -1,3 +1,5 @@
-TRUNCATE TABLE `materialinfotype`;
-INSERT INTO `materialinfotype` (`id`, `name`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
-(1, 'ingredient', '', 1, '2015-10-19 12:08:08', 10, '2015-10-19 01:08:08', 10);
+TRUNCATE TABLE `productinfotype`;
+INSERT INTO `productinfotype` (`id`, `name`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
+(1, 'Material', 'Material Ref', 1, '2015-10-19 12:08:08', 10, '2015-10-19 01:08:08', 10),
+(2, 'Category', 'Category Ref', 1, '2015-10-19 12:08:08', 10, '2015-10-19 01:08:08', 10),
+(3, 'Store', 'Store Ref', 1, '2015-10-19 12:08:08', 10, '2015-10-19 01:08:08', 10);
