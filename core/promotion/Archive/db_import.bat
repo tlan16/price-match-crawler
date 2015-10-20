2503a4360b6bbc6a4c4a74a1b9510f4474ec6192
lhe <lhe@bytecraft.com.au>
2015-10-20 11:30:49 +1100

    windows shell

---
+++ core/promotion/Archive/db_import.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+SET BASEDIR=%~dp0
+SET FPASSWORD=
+SET DBNAME=sushico
+SET DBHOST=localhost
+SET DBUSERNAME=root
+SET DBPASSWORD=
+SET MYSQLPATH=C:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe
+
+echo Directory: %BASEDIR%
+echo DatabaseName: %DBNAME%
+
+echo create database %DBNAME% if not exists
+%MYSQLPATH% -h %DBHOST% -u %DBUSERNAME% -p%DBPASSWORD% -e "CREATE DATABASE IF NOT EXISTS %DBNAME% DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+
+echo import sql files
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../structure.sql
+%$MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../useraccount.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../person.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../role.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../userprofiletype.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../userprofile.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../systemsettings.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../ingredientinfotype.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../materialinfotype.sql
+%MYSQLPATH% -h%DBHOST% -u%DBUSERNAME% -p%DBPASSWORD% %DBNAME% < %BASEDIR%/../allergent.sql
+
+echo done
