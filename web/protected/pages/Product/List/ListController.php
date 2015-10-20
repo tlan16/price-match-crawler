ef0ece2da3c317f05cc01ffcddc92c3021b914c4
helin16 <helin16@192.168.200.202>
2015-10-20 20:57:02 +1100

    added Product listing and details page

---
+++ web/protected/pages/Product/List/ListController.php
@@ -0,0 +1,61 @@
+<?php
+
+class ListController extends CRUDPageAbstract
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_focusEntity = 'Product';
+	}
+	/**
+	 * (non-PHPdoc)
+	 * @see CRUDPageAbstract::_getEndJs()
+	 */
+	protected function _getEndJs()
+	{
+		$js = parent::_getEndJs();
+		$js .= "pageJs.getResults(true, " . $this->pageSize . ");";
+		$js .= "pageJs.loadSelect2();";
+		$js .= "pageJs._bindSearchKey();";
+		$js .= 'pageJs.setCallbackId("updateItem", "' . $this->updateItemBtn->getUniqueID(). '");';
+		return $js;
+	}
+	/**
+	 * Getting the items
+	 *
+	 * @param unknown $sender
+	 * @param unknown $param
+	 * @throws Exception
+	 *
+	 */
+	public function getItems($sender, $param)
+	{
+		$results = $errors = array();
+		try
+		{
+			$class = trim($this->_focusEntity);
+			$pageNo = 1;
+			$pageSize = DaoQuery::DEFAUTL_PAGE_SIZE;
+			if(isset($param->CallbackParameter->pagination))
+			{
+				$pageNo = $param->CallbackParameter->pagination->pageNo;
+				$pageSize = $param->CallbackParameter->pagination->pageSize;
+			}
+			$stats = array();
+			$class::getQuery()->eagerLoad('Product.infos', 'inner join', 'pro_info', '(pro_info.productId = pro.id and pro_info.active = 1 and pro_info.typeId = ? and pro_info.entityName = ? and pro_info.entityId = ?)');
+			$objects = $class::getAllByCriteria('pro.active = 1', array(ProductInfoType::ID_STORE, ProductInfoType::ENTITY_NAME_STORE, Core::getStore()->getId()), true, $pageNo, $pageSize, array(), $stats);
+			
+			$results['pageStats'] = $stats;
+			$results['items'] = array();
+			foreach($objects as $obj)
+				$results['items'][] = $obj->getJson();
+		}
+		catch(Exception $ex)
+		{
+			$errors[] = $ex->getMessage();
+		}
+		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+	}
+	
+	
+}
