53801b3bba4d8c15b261bff6e4d575257634de62
Frank <tlan16@sina.com>
2015-10-20 19:41:54 +1100

    rename test.php

--- web/test.php
+++ web/test.php
@@ -17,36 +17,12 @@ try {
 	$transStarted = false;
 	try {Dao::beginTransaction();} catch(Exception $e) {$transStarted = true;}
 
-	$materials = createEntity('Material', 4);
-	$ingredients = createEntity('Ingredient', 3);
-	$allergents = createEntity('Allergent', 2);
-	$serveMeasurements = createEntity('ServeMeasurement', 3);
-	$nutritions = createEntity('Nutrition', 3);
-
-	//create ingredient
-	foreach($materials as $material) {
-	    foreach($ingredients as $ingredient) {
-    	    foreach($allergents as $allergent) {
-    	        $ingredient->addAllergent($allergent);
-    	    }
-    	    $material->addIngredient($ingredient);
-    	    foreach($nutritions as $nutrition) {
-        	    foreach($serveMeasurements as $index => $serveMeasurement) {
-        	        $material->addNutrition($nutrition, $index, $serveMeasurement);
-        	    }
-    	    }
-	    }
-	}
-
-	$obj = Product::createWithParams('test', 'test description', '987654321', '123', '+3 day', '23.40', 'v1', Material::getAll());
-	$newLabel = null;
-	$obj->printLabel(null, null, $newLabel);
-	$imgPath = $newLabel->generateImg(200, 600);
-	header('Content-Type: image/jpeg');
-	echo file_get_contents($imgPath);
+	$obj = Material::create('testMaterial1', 'des1');
+	$obj->addIngredient(Ingredient::get(1));
+	$obj->addNutrition(Nutrition::get(1), 10, ServeMeasurement::get(1));
 
 	if($transStarted === false)
-		Dao::rollbackTransaction();
+		Dao::commitTransaction();
 } catch (Exception $ex) {
 	if($transStarted === false)
 			Dao::rollbackTransaction();
---
+++ web/testPrinting.php
@@ -0,0 +1,54 @@
+<?php
+require_once dirname(__FILE__) . '/bootstrap.php';
+Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
+
+function createEntity($class, $howMany) {
+    $return = array();
+    for($i = 0; $i < $howMany; $i++) {
+        $entity = new $class();
+        $return[] = $entity->setName(substr($class, 0, 1) . '_' . $i)
+            ->setDescription('')
+            ->save();
+    }
+    return $return;
+}
+
+try {
+	$transStarted = false;
+	try {Dao::beginTransaction();} catch(Exception $e) {$transStarted = true;}
+
+	$materials = createEntity('Material', 4);
+	$ingredients = createEntity('Ingredient', 3);
+	$allergents = createEntity('Allergent', 2);
+	$serveMeasurements = createEntity('ServeMeasurement', 3);
+	$nutritions = createEntity('Nutrition', 3);
+
+	//create ingredient
+	foreach($materials as $material) {
+	    foreach($ingredients as $ingredient) {
+    	    foreach($allergents as $allergent) {
+    	        $ingredient->addAllergent($allergent);
+    	    }
+    	    $material->addIngredient($ingredient);
+    	    foreach($nutritions as $nutrition) {
+        	    foreach($serveMeasurements as $index => $serveMeasurement) {
+        	        $material->addNutrition($nutrition, $index, $serveMeasurement);
+        	    }
+    	    }
+	    }
+	}
+
+	$obj = Product::createWithParams('test', 'test description', '987654321', '123', '+3 day', '23.40', 'v1', Material::getAll());
+	$newLabel = null;
+	$obj->printLabel(null, null, $newLabel);
+	$imgPath = $newLabel->generateImg(200, 600);
+	header('Content-Type: image/jpeg');
+	echo file_get_contents($imgPath);
+
+	if($transStarted === false)
+		Dao::rollbackTransaction();
+} catch (Exception $ex) {
+	if($transStarted === false)
+			Dao::rollbackTransaction();
+	throw $ex;
+}
