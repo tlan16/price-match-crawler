e41f05fc2c3101c8b761352d95b410e842e07886
lhe <lhe@bytecraft.com.au>
2015-10-20 15:53:42 +1100

    added qrcode library

--- web/test.php
+++ web/test.php
@@ -2,15 +2,51 @@
 require_once dirname(__FILE__) . '/bootstrap.php';
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 
+function createEntity($class, $howMany) {
+    $return = array();
+    for($i = 0; $i < $howMany; $i++) {
+        $entity = new $class();
+        $return[] = $entity->setName(substr($class, 0, 1) . '_' . $i)
+            ->setDescription('')
+            ->save();
+    }
+    return $return;
+}
+
 try {
 	$transStarted = false;
 	try {Dao::beginTransaction();} catch(Exception $e) {$transStarted = true;}
 
-	$obj = Address::create('street', 'city', 'region', 'country', 1234);
-	var_dump($obj);
-	
+	$materials = createEntity('Material', 4);
+	$ingredients = createEntity('Ingredient', 3);
+	$allergents = createEntity('Allergent', 2);
+	$serveMeasurements = createEntity('ServeMeasurement', 3);
+	$nutritions = createEntity('Nutrition', 3);
+
+	//create ingredient
+	foreach($materials as $material) {
+	    foreach($ingredients as $ingredient) {
+    	    foreach($allergents as $allergent) {
+    	        $ingredient->addAllergent($allergent);
+    	    }
+    	    $material->addIngredient($ingredient);
+    	    foreach($nutritions as $nutrition) {
+        	    foreach($serveMeasurements as $index => $serveMeasurement) {
+        	        $material->addNutrition($nutrition, $index, $serveMeasurement);
+        	    }
+    	    }
+	    }
+	}
+
+	$obj = Product::createWithParams('test', 'test description', '987654321', '123', '+3 day', '23.40', 'v1', Material::getAll());
+	$newLabel = null;
+	$obj->printLabel(null, null, $newLabel);
+	$imgPath = $newLabel->generateImg(200, 600);
+	header('Content-Type: image/jpeg');
+	echo file_get_contents($imgPath);
+
 	if($transStarted === false)
-		Dao::commitTransaction();
+		Dao::rollbackTransaction();
 } catch (Exception $ex) {
 	if($transStarted === false)
 			Dao::rollbackTransaction();
