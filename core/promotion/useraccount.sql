17ad7cad19776511164a9af3d7ee4eb441b26112
Frank <tlan16@sina.com>
2015-10-26 14:35:52 +1100

    update predata for useraccount for new hash method

--- core/promotion/useraccount.sql
+++ core/promotion/useraccount.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE `useraccount`;
-INSERT INTO `useraccount` (`id`, `username`, `password`, `personId`, `source`, `refId`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
-(10, '075ae3d2fc31640504f814f60e5ef713', 'disabled', 10, NULL, NULL, 1, '2014-03-06 19:47:35', 10, '2014-03-05 10:47:35', 10),
-(24, 'frank', 'b85b2c37a170c7fa6100dcca17ba66d370207744', 24, NULL, NULL, 1, '2014-12-20 13:23:44', 20, '2015-02-04 14:48:00', 20),
-(25, 'testuser', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 2514, NULL, NULL, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24),
-(26, 'Mushfique', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 25, NULL, NULL, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24),
-(27, 'helin16', 'd6e4689faedbe2cb3831853d9cdded1a8ccb1332', 26, NULL, NULL, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24);
+INSERT INTO `useraccount` (`id`, `username`, `password`, `personId`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
+(10, '075ae3d2fc31640504f814f60e5ef713', 'disabled', 10, 1, '2014-03-06 19:47:35', 10, '2014-03-05 10:47:35', 10),
+(24, 'frank', '$2y$10$QmKNZ.dHUeCYwfgKoq.WX.OuIn7tyt0i.L..qj0plDUTWE.0KAAQ.', 24, 1, '2014-12-20 13:23:44', 20, '2015-02-04 14:48:00', 20),
+(25, 'testuser', '$2y$10$1jQO1uN3Rewy/jQn1Mn9puiRZQ96tqYydxAJ/bFOELpdMiQ.oK76q', 2514, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24),
+(26, 'Mushfique', '$2y$10$wpTV2e53T4DirUXWqR7aWeuQ3u8ToZ84CaDjKj/BzWz9qKSmyXu.m', 25, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24),
+(27, 'helin16', '$2y$10$goC0i9VPTsoAVKEhl0ViGux33k9d/z/lv9G2CR7x5CMcsxFXgMnNW', 26, 1, '2015-09-03 10:43:17', 24, '2015-09-02 14:43:17', 24);
