13024fc04565abd861eb4e4ffb836e138f89089c
Frank <tlan16@sina.com>
2015-10-30 20:58:20 +1100

    demo sql

---
+++ core/promotion/demo.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE `useraccount`;
+INSERT INTO `useraccount` (`id`, `username`, `password`, `personId`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
+(10, '075ae3d2fc31640504f814f60e5ef713', 'disabled', 10, 1, '2014-03-06 19:47:35', 10, '2014-03-04 23:47:35', 10),
+(24, 'testuser', '$2y$10$VVNd/onZnHYwuigZ1uOHWu4C44JrjA1KqTXOAouqji45ZYCbFPdxS', 24, 1, '2014-12-20 13:23:44', 10, '2015-10-30 09:55:47', 10);
