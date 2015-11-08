446d0d99c4c559c9b23c06dc7a4449344506cb99
Frank <tlan16@sina.com>
2015-11-09 10:38:47 +1100

    bug fix

--- core/promotion/Archive/getDatabaseSize.sh
+++ core/promotion/Archive/getDatabaseSize.sh
@@ -7,4 +7,9 @@ DBUSERNAME=root
 DBPASSWORD=root
 MYSQLPATH=mysql
 
-$MYSQLPATH -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD -e 'SELECT table_schema "$DBUSERNAME" Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema;'
+$MYSQLPATH -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD -e '
+SELECT table_schema                                        "DB Name", 
+   Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" 
+FROM   information_schema.tables 
+GROUP  BY table_schema; 
+'
