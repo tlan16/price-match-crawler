2904ca95218ec548685ba59dcad094c5d4f73609
Frank <tlan16@sina.com>
2015-11-09 10:34:58 +1100

    bug fix

--- core/promotion/Archive/getDatabaseSize.sh
+++ core/promotion/Archive/getDatabaseSize.sh
@@ -7,4 +7,4 @@ DBUSERNAME=root
 DBPASSWORD=root
 MYSQLPATH=mysql
 
-$MYSQLPATH -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD -e "SELECT price_match_crawler Round(Sum(data_length + index_length) / 1024 / 1024, 1) FROM information_schema.tables GROUP  BY price_match_crawler;" 
+$MYSQLPATH -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD -e 'SELECT table_schema "$DBUSERNAME" Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema;" 
