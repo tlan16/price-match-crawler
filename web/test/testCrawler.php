8fa16608585daad47589354c3c27d40094939a95
Frank <tlan16@sina.com>
2015-11-09 06:07:10 +1100

    test

--- web/test/testCrawler.php
+++ web/test/testCrawler.php
@@ -2,13 +2,13 @@
 require_once dirname(__FILE__) . '/testAbstract.php';
 ini_set('memory_limit','2048M');
 
+echo basename(__FILE__) . '::$argv' . print_r($argv, true) . PHP_EOL;
+
 class testCrawler extends testAbstract
 {
 	public static function run($debug = true)
 	{
 		parent::run();
-		
-		echo __CLASS__ . '::' . __FUNCTION__ . '::$argv' . print_r($argv, true) . PHP_EOL;
 		
 		if(isset($argv) && isset($argv[1]) && ($productId = intval($argv[1])) !== 0 && ($product = Product::get($productId)) instanceof Product)
 		{
