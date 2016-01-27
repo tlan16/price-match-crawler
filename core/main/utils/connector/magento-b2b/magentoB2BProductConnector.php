2b1c4a2c1acace00a9b058b7066ca4d0d3fdf020
Larry Yu <yuxiaofengch@hotmail.com>
2016-01-27 22:04:41 +1100

    fix bug for search condition

--- core/main/utils/connector/magento-b2b/magentoB2BProductConnector.php
+++ core/main/utils/connector/magento-b2b/magentoB2BProductConnector.php
@@ -20,12 +20,12 @@ class magentoB2BProductConnector extends magentoB2BconnectorAbstract
 			$product = Product::create($sku, isset($data['shortDescription']) ? $data['shortDescription'] : '');
 			SystemSettings::getByType('last_succ_product_sync')->setValue($data['updated'])->save();
 			if($this->debug)
-				self::log('DONE Product[' . $product->getId() . '] ' . $product->getSku() . 'updated : ' . $data['updated']);
+				self::log('DONE Product[' . $product->getId() . '] ' . $product->getSku() . ' updated : ' . $data['updated']);
 		}
 	}
 	public static function importProducts($baseurl, $username, $password, $debug = false) 
 	{
 		$connector = new magentoB2BProductConnector($baseurl, $username, $password, $debug = true);
-		$connector->getAllByCriteria($connector->baseurl . $connector->prefix, 'updated >= "' . SystemSettings::getByType('last_succ_product_sync')->getValue(). '"') ;
+		$connector->getAllByCriteria($connector->baseurl . $connector->prefix, 'updated > "' . SystemSettings::getByType('last_succ_product_sync')->getValue(). '"') ;
 	}
 }
